b4c2b7c0221c130d1920b8bacfe29e2805a8e26e
samuf <farkas.david05@gmail.com>
2019-02-14 17:27:46 +0100

    VMS-14(feat)Mailing / added own google secret and id, set os enviroment variables for mailing, needs to restart the machine

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -16,8 +16,8 @@ db = SQLAlchemy(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config['MAIL_USERNAME'] = os.environ.get('USER_EMAIL')
+app.config['MAIL_PASSWORD'] = os.environ.get('USER_PASSWORD')
 mail = Mail(app)
 
 from flaskr import routes
