ea3d3ac9aa063a66d392416e7cab5626c0d198a9
samuf <farkas.david05@gmail.com>
2019-02-25 10:06:52 +0100

    Added info and files, docker ready verison.

---
+++ instance/config.py
@@ -0,0 +1,13 @@
+GOOGLE_ID = 'Your google app ID'
+
+GOOGLE_SECRET = 'Google secret'
+
+USER_GROUPS = ['viewer', 'employee', 'administrator']
+
+REQUESTS_PER_PAGE_ADMIN = 5
+
+REQUESTS_PER_PAGE = 10
+
+USER_EMAIL = 'Your email address'
+
+USER_PW = 'Your email password'
