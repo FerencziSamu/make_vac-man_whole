150650becb274fb1ddb7df59b3d296442f07a2af
samuf <farkas.david05@gmail.com>
2019-03-04 14:47:14 +0100

    VMS-4_Test / reworking get_days_left

--- flaskr/tests/test_methods.py
+++ flaskr/tests/test_methods.py
@@ -263,13 +263,13 @@ def test_get_days_left():
         # With proper user
         p = routes.get_days_left(fake_user)
         assert p == 20
-        # With 25 taken days out from 20
+        # With created leave request for 6 days
         url = "http://127.0.0.1:5000/save_request"
         data = {"current_user": "test@invenshure.com", "start-date": "03/14/2019", "end-date": "03/19/2019"}
         requests.post(url, data)
+        db.commit()
         q = routes.get_days_left(fake_user)
-        assert q == 20
-
+        assert q == 14
         # With random email from db
         with pytest.raises(AttributeError):
             routes.get_days_left("test@invenshure.com")
@@ -286,10 +286,12 @@ def test_get_days_left():
         with pytest.raises(AttributeError):
             routes.get_days_left(fake_category)
     finally:
+        pass
         # Removing test user and test category
         fake_user = routes.User.query.filter_by(email="test@invenshure.com").first()
         fake_category = routes.LeaveCategory.query.filter_by(category="test_test_1").first()
         fake_request = routes.LeaveRequest.query.filter_by(user_id=fake_user.id).first()
+        logging.info(str(fake_user.days) + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         db.delete(fake_request)
         db.delete(fake_user)
         db.delete(fake_category)
